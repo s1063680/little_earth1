41b18bd17161e794351f96a1be10daa05b8778cc
eatfish <s1063680@gm.pu.edu.tw>
2020-06-17 16:51:42 +0800

    圈差產生

--- app/src/main/java/tw/edu/pu/s1063680/little_earth/GameActivity2.java
+++ app/src/main/java/tw/edu/pu/s1063680/little_earth/GameActivity2.java
@@ -6,12 +6,16 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import java.util.Random;
 
 public class GameActivity2 extends AppCompatActivity {
+    Random rand = new Random();
+    int randNumber;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +35,15 @@ public class GameActivity2 extends AppCompatActivity {
         //给按钮rollButton设置点击监听器，一旦用户点击按钮，就触发监听器的onClick方法
         button9.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                int prerandNumber = randNumber;
+                //                产生随机数
+                randNumber = rand.nextInt(5) + 1;
+                //String randNumber2 = rand.nextInt(5)+1;
 
-//                产生随机数
-                Random rand = new Random();
-                int randNumber = rand.nextInt(5) + 1;
-//                String randNumber2 = rand.nextInt(5)+1;
+                ImageView image14 = (ImageView) findViewById(R.id.imageView14);
+                if(randNumber != prerandNumber){
+                    image14.setImageResource(R.drawable.empty);
+                }
 
 //                获取对ImageView对象的引用
                 ImageView diceImage = findViewById(R.id.dice_image);
@@ -69,11 +77,58 @@ public class GameActivity2 extends AppCompatActivity {
                 diceImage.setImageDrawable(drawable);
             }
         });
-    }
-    public void backgame(View v){
-        Intent it = new Intent();
-        it.setClass(this, GameActivity1.class);
-        startActivity(it);
-        finish();
-    }
+        Button button15 = (Button) findViewById(R.id.button15);
+        button15.setOnClickListener(new OnClickListener() {
+            ImageView image14 = (ImageView) findViewById(R.id.imageView14);
+            @Override
+            public void onClick(View arg0) {
+                arg0.bringToFront();
+
+                boolean clickTrashCan = false;
+
+                // 按下按鈕,來切換圖片
+                if (!clickTrashCan) {
+
+                    if (randNumber == 1 || randNumber == 4 || randNumber == 5) {
+                        image14.setImageResource(R.drawable.correct);
+                        clickTrashCan = true;
+                    }else {
+                        image14.setImageResource(R.drawable.error);
+                        clickTrashCan = false;
+                    }
+                }
+
+            }
+
+        });
+        Button button16 = (Button) findViewById(R.id.button16);
+        button16.setOnClickListener(new OnClickListener() {
+            ImageView image14 = (ImageView) findViewById(R.id.imageView14);
+            @Override
+            public void onClick(View arg0) {
+                arg0.bringToFront();
+
+                boolean clickTrashCan = false;
+
+                // 按下按鈕,來切換圖片
+                if (!clickTrashCan) {
+
+                    if (randNumber == 2 || randNumber == 3) {
+                        image14.setImageResource(R.drawable.correct);
+                        clickTrashCan = true;
+                    }else {
+                        image14.setImageResource(R.drawable.error);
+                        clickTrashCan = false;
+                    }
+                }
+            }
+
+        });
+        }
+    public void backgame (View v){
+            Intent it = new Intent();
+            it.setClass(this, GameActivity1.class);
+            startActivity(it);
+            finish();
+        }
 }
